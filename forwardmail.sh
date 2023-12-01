0ea272b5b3240937fced405a6eecbc0bc87aeb15
petergeelhoed <petergeelhoed@gmail.com>
2023-12-01 08:32:07 +0100

    proper forwarding

--- forwardmail.sh
+++ forwardmail.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-# takes an integer to get the mail from your inbox
- [[ -z "$1" ]] && exit 
-  rm body tmpmail ; 
-echo -e "w $1 body\ns $1 tmpmail \n"  | mail -f /home/peter/mbox
+check=$(/usr/bin/mail -H 2>&1 )
+if [[ ${check} == "No applicable messages" ]] 
+then 
+exit 0
+fi
+
+for i in $(/usr/bin/mail -H | grep "^[> ][N]" | awk '{print $2}' | tac ); 
+do
+ [[ -e tmpmail ]] && rm tmpmail
+ [[ -e body ]] && rm body
+ echo -e "w $i body\ns $i tmpmail \n p $i\n"  | /usr/bin/mail > /dev/null
  [[ -e tmpmail ]] || exit 
  [[ -e body ]] || exit 
  [[ -e header ]] && rm header
 
-from=$(sed 's/^From: //p' -n tmpmail | head -n 1 ); 
-#subject=$(sed 's/^Subject: //p;' -n tmpmail |  tr -c '[A-z 0-9:\n]' '_' | head -n 1 ); 
 
  formail -X 'From:' < tmpmail | sed 's/From: //;s/ <[^ ]*>//;s/<\|>//g;s/@/ /;s/^/From: /;s/$/ <peter@petergeelhoed.nl>/' > header
+ formail -X 'Subject'< tmpmail  | tr -c '[A-z 0-9:\n]' '_' | head -n 1 >> header
  formail -X 'Content-Type'< tmpmail >> header
  formail -X 'Content-Disposition' < tmpmail >> header
  formail -X 'Content-Transfer-Encoding'< tmpmail >> header
-subject=$(formail -x 'Subject'< tmpmail  | tr -c '[A-z 0-9:\n]' '_' | head -n 1 )
 
-b64=$(echo "$header" | grep "Content-Transfer-Encoding: base64" | wc -l)
-if [[ "${b64}" -gt 0 ]]
-then
-base64 < body |  mail -a "$(cat header)" -s "$subject" petergeelhoed@delta.nl 
-else
+ b64=$(echo "$header" | grep "Content-Transfer-Encoding: base64" | wc -l)
+ if [[ "${b64}" -gt 0 ]]
+ then
+ base64 < body |  mail -a "$(cat header)" -s "$subject" petergeelhoed@delta.nl 
+ else
 
 #cat body |  mail -a "${header}" -s "forward" petergeelhoed@delta.nl 
 #echo   mail -a "$(cat header)" -s "$subject" petergeelhoed@delta.nl 
-cat body |  mail -a "$(cat header)" -s "$subject" petergeelhoed@delta.nl 
-#cat body |  mail -a "$(cat header)" -s "$subject" charon
-fi
+cat body |  mail -a "$(cat header)"  petergeelhoed@delta.nl 
+# cat body |  mail -a "$(cat header)" charon
+ cat header
+ fi
+done
