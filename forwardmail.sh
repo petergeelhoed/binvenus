8803f9192b797d9cd37587f36b30eaa0a96a0191
petergeelhoed <petergeelhoed@gmail.com>
2023-12-05 14:23:53 +0100

    checkip fwd

--- forwardmail.sh
+++ forwardmail.sh
@@ -14,12 +14,17 @@ do
  [[ -e body ]] || exit 
  [[ -e header ]] && rm header
 
+ # do not forward this
+ [[ $(formail -X 'From:' <  tmpmail | grep "From: Mail Delivery System" | wc -l) -gt 0 ]] && exit
+ [[ $(formail -X 'Subject'< tmpmail | grep "Subject: Report domain:" | wc -l) -gt 0 ]] && exit
 
  formail -X 'From:' < tmpmail | sed 's/From: //;s/ <[^ ]*>//;s/<\|>//g;s/@/ /;s/^/From: /;s/$/ <peter@petergeelhoed.nl>/' > header
  formail -X 'Subject'< tmpmail  | tr -c '[A-z 0-9:\n]' '_' | head -n 1 >> header
  formail -X 'Content-Type'< tmpmail >> header
  formail -X 'Content-Disposition' < tmpmail >> header
  formail -X 'Content-Transfer-Encoding'< tmpmail >> header
+
+
 
  b64=$(echo "$header" | grep "Content-Transfer-Encoding: base64" | wc -l)
  if [[ "${b64}" -gt 0 ]]
